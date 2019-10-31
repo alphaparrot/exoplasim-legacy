d461842137984bc7b16caf3bdea89dca0822fc80
Adiv Paradise <paradise@astro.utoronto.ca>
2019-10-31 16:57:17 -0400

    Changes to no-ice mode, and added an array-reading utility to utilities.f90 (prepping for the future)

--- plasim/src/utilities.f90
+++ plasim/src/utilities.f90
@@ -258,6 +258,32 @@
       
       return
       end subroutine readarray      
+
+!--------------------------------------------------------------------72
+!
+! Read a 2D array from a formatted text file with single-line header
+      
+      subroutine readdat(filename,ndim,nitems,kdata)
+      
+      character (len=*), intent(in) :: filename
+      character (len=128) :: header
+      integer, intent(in) :: ndim
+      integer, intent(in) :: nitems
+      real, intent(out) :: kdata(nitems,ndim)
+      
+      
+      open(87,file=filename,form='formatted')
+      read(87,'(a)') header
+      
+      do nl = 1,nitems
+         read(87,*) kdata(nl,:)
+      enddo
+      
+      close(87)
+      
+      
+      return
+      end subroutine readdat      
       
 ! >>>--ayp      
             
