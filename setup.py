006d2b0773297567caf509c0263e776a9470a630
Adiv Paradise <paradise@astro.utoronto.ca>
2020-12-02 23:44:13 -0500

    Trying a new variant

--- setup.py
+++ setup.py
@@ -4,27 +4,26 @@ import atexit
 
 from setuptools.command.install import install                                       
     
-
-class CustomInstall(install):                                                       
-    def run(self):
-        def _post_install():
-            def find_module_path():
-                for p in sys.path:
-                    if os.path.isdir(p) and "exoplasim" in os.listdir(p):
-                        return os.path.join(p, "exoplasim")
-            install_path = find_module_path()
-            cwd=os.getcwd()
-            os.chdir(install_path)
-            os.system("./configure.sh")
-            os.system("echo %s>>__init__.py"%install_path)
-            os.chdir(cwd)
-
+def _post_install():
+   def find_module_path():
+       for p in sys.path:
+           if os.path.isdir(p) and "exoplasim" in os.listdir(p):
+               return os.path.join(p, "exoplasim")
+   install_path = find_module_path()
+   cwd=os.getcwd()
+   os.chdir(install_path)
+   os.system("./configure.sh")
+   os.system("echo %s>>__init__.py"%install_path)
+   os.chdir(cwd)
+   
+class CustomInstall(install): 
+    def __init__(self, *args, **kwargs):
+        super(CustomInstall, self).__init__(*args, **kwargs)
         atexit.register(_post_install)
-        install.run(self)
 
 setup(
     name='exoplasim',
-    version='2.0.0.post2',
+    version='2.0.0.post3',
     packages=['exoplasim',],
     install_requires=["numpy","netCDF4"],
     include_package_data=True,
