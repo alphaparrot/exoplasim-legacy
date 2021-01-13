9de75c70b4e18579c523d31deb948775640e9a33
Adiv Paradise <paradise@astro.utoronto.ca>
2021-01-13 15:07:48 -0500

    Update metadata

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
  
 setup(
     name='exoplasim',
-    version='2.0.8',
+    version='2.0.9',
     packages=['exoplasim',],
     zip_safe=False,
     install_requires=["numpy","netCDF4","matplotlib","scipy"],
