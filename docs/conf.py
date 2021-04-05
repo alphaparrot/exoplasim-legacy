5d20c99970287828bda47f53fb23b46fdabf0515
Adiv Paradise <paradise@astro.utoronto.ca>
2021-04-05 16:35:16 -0400

    Updated version number

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2020, Adiv Paradise'
 author = 'Adiv Paradise'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.0'
+release = '2.2.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
  
 setup(
     name='exoplasim',
-    version='2.2.0',
+    version='2.2.1',
     packages=['exoplasim',],
     zip_safe=False,
     install_requires=["numpy","netCDF4","matplotlib","scipy"],
