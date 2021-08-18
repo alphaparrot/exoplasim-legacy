09283d395bfaf827cd244ea623f34a7aa932dc56
Adiv Paradise <paradise@astro.utoronto.ca>
2021-08-18 00:09:46 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
  
 setup(
     name='exoplasim',
-    version='3.0.0a4',
+    version='3.0.0a5',
     packages=['exoplasim',],
     zip_safe=False,
     install_requires=["numpy","matplotlib","scipy"],
